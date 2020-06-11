e80de1411b2d0fd0d8a30203c80c8a6d3f8a9d7c
bsquillin <bradquillin@gmail.com>
2020-06-11 19:49:14 +0000

    betty stylized

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 #include <time.h>
 
-/*
-*  main - find if number is neg or pos
-* return = 0
+/**
+ * main - find if number is neg or pos
+ * Return: 0
  */
 
 int main(void)
@@ -13,12 +13,18 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	 if (n == 0)
-{ printf("%d is 0\n", n);
-   } else if (n > 0)
-{ printf("%d is positive\n", n);
-   } else if (n < 0)
-{ printf("%d is negative\n", n); }
+	if (n == 0)
+	{
+		printf("%d is 0\n", n);
+	}
+	else if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
 
-return (0);
+	return (0);
 }
