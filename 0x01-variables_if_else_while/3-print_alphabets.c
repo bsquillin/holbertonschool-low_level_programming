4d15532a7b310701563c891df286a4ebadc27ac2
bsquillin <bradquillin@gmail.com>
2020-06-12 03:57:03 +0000

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,20 @@
-#include <stdio.h> 4-print_alphabt.c 5-print_numbers.c 6-print_numberz.c 7-print_tebahpla.c 8-print_base16.c 9-print_comb.c 10-print_comb2.c
+#include <stdio.h>
+
+/**
+ *main - alphabet
+ *Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
