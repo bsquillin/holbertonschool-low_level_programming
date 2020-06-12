9a30fa08ad0a53522bb99798313a79bf331e040f
bsquillin <bradquillin@gmail.com>
2020-06-12 04:03:32 +0000

    added new line

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,5 +12,8 @@ int main(void)
 	{
 		putchar(ch);
 	}
+	{
+		putchar('\n');
+	}
 	return (0);
 }
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -16,5 +16,8 @@ int main(void)
 	{
 		putchar(ch);
 	}
+	{
+		putchar('\n');
+	}
 	return (0);
 }
