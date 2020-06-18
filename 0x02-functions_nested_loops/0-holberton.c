c06be78c030a97aae39cfad9c709d7f41a8c6e97
bsquillin <66336560+bsquillin@users.noreply.github.com>
2020-06-18 00:58:49 -0500

    Create 0-holberton.c

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,23 @@
+#include <holberton.h>
+#include <stdio.h>
+
+/**
+ *main - putchar holberton
+ *Return: 0
+ */
+ 
+ int main()
+ {
+  _putchar("H");
+  _putchar("o");
+  _putchar("l");
+  _putchar("b");
+  _putchar("e");
+  _putchar("r");
+  _putchar("t");
+  _putchar("o");
+  _putchar("n");
+  _putchar("\n");
+
+  return 0;
+  }
