49fbfeff2fac8425db8917d9d344b0b6b6ced60f
bsquillin <66336560+bsquillin@users.noreply.github.com>
2020-06-21 14:06:36 -0500

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,19 @@
 #include "holberton.h"
 /**
- * _islower - checks for lowercase letters
+ * _isalpha - checks for lowercase letters
  * @c: character to check
  *
  * Return: 0 or 1
  */
 
-int _islower(int c)
+int _isalpha(int c)
 {
 	if (c >= 'a' && c <= 'z')
+
 		return (1);
+
 	else
+
 		return (0);
+
 }
