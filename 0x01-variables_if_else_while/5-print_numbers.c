53f956e4743200b4b37a7b68e46639b3db2234db
bsquillin <66336560+bsquillin@users.noreply.github.com>
2020-06-21 07:13:12 -0500

    reorganized

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - printing numbers
+ *
+*Return: 0
+ */
+int main(void)
+
+{
+
+        printf("0123456789\n");
+
+        return (0);
+
+}
