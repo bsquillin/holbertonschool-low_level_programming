50ec5e35d639594b3ab880e3131a9f83e4c0f8ed
bsquillin <66336560+bsquillin@users.noreply.github.com>
2020-06-21 09:59:05 -0500

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,14 +6,14 @@
  */
  int main(void)
  {
-  char c;
+   char c;
   
-  c = 'a';
-  while (c<= 'z')
+   c = 'a';
+  while (c <= 'z')
   {
     _putchar(c);
     c++;
     }
     _putchar('\n');
-    return 0
+    return (0);
    }
