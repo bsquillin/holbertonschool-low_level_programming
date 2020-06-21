c68dff8b4e4d40f6cf40f98e75e41eff185c6251
bsquillin <66336560+bsquillin@users.noreply.github.com>
2020-06-21 09:55:33 -0500

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * main - prints alphabet
+ *
+ *Return: 0
+ */
+ int main(void)
+ {
+  char c;
+  
+  c = 'a';
+  while (c<= 'z')
+  {
+    _putchar(c);
+    c++;
+    }
+    _putchar('\n');
+    return 0
+   }
