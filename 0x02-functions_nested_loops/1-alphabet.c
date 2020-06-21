332191e6ddaf3f473a159b5fe3eef119f926acf2
bsquillin <66336560+bsquillin@users.noreply.github.com>
2020-06-21 12:34:34 -0500

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,4 @@
 #include "holberton.h"
-<<<<<<< HEAD
-
 /**
  * main - printing the alphabet with _putchar
  *
@@ -17,25 +15,5 @@ void print_alphabet(void)
 	}
 
 	_putchar('\n');
-
+	return 0;
 }
-=======
-/**
- * main - prints alphabet
- *
- *Return: 0
- */
- int main(void)
- {
-   char c;
-  
-   c = 'a';
-  while (c <= 'z')
-  {
-    _putchar(c);
-    c++;
-    }
-    _putchar('\n');
-    return (0);
-   }
->>>>>>> 50ec5e35d639594b3ab880e3131a9f83e4c0f8ed
