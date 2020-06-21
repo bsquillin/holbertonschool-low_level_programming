f8d2268b44f46a19e8a10638b71e7e19d6a489d3
bsquillin <66336560+bsquillin@users.noreply.github.com>
2020-06-21 13:20:19 -0500

    Update 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,18 +1,14 @@
-<<<<<<< HEAD
 #include "holberton.h"
-
 /**
  *main - putchar holberton
  *Return: 0
  */
 
 int main(void)
-
 {
 	int i = 0;
 	char c;
 	char s[] = "Holberton\n";
-
 	while (i <= 9)
 	{
 		c = s[i];
@@ -22,30 +18,4 @@ int main(void)
 
 	return (0);
 
-=======
-#include <holberton.h>
-#include <stdio.h>
-
-/**
- *main - putchar holberton
- *Return: void
- */
- 
-int main(void)
-{
-  char c;
- {
-  _putchar('H');
-  _putchar('o');
-  _putchar('l');
-  _putchar('b');
-  _putchar('e');
-  _putchar('r');
-  _putchar('t');
-  _putchar('o');
-  _putchar('n');
-  _putchar('\n';
-  }
-  return 0;
->>>>>>> 50ec5e35d639594b3ab880e3131a9f83e4c0f8ed
 }
