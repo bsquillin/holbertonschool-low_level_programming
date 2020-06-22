4896cb890521240088bc6d347b44b5c5475dbe38
bsquillin <bradquillin@gmail.com>
2020-06-22 16:22:25 +0000

    first commit

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _isupper(int c);
+int _isalpha(int c);
+int _islower(int c);
+int _putchar(char c);
+void positive_or_negative(void);
+
+#endif /* HOLBERTON_H */
