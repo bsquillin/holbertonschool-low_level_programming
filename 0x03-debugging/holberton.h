1c39e6ebbaf3107e0295388df86e1c4fa230d85f
bsquillin <bradquillin@gmail.com>
2020-06-23 04:43:57 +0000

    added

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -2,7 +2,9 @@
 #define HOLBERTON_H
 
 #include <stdio.h>
-int _putchar(char c)
+
+int largest_number(int a, int b, int c);
+int _putchar(char c);
 void positive_or_negative(int i);
 
 #endif /* HOLBERTON_H */
